9dfcd3f7b6babb5ad086c6c961496b378cd565a5
Elle H <itsellej@gmail.com>
2018-08-05 14:23:23 +0100

    Add celsius converter function and event listener

--- main.js
+++ main.js
@@ -2,5 +2,21 @@ const celsiusInput = document.querySelector("#celsius > input");
 const fahrenheitInput = document.querySelector("#fahrenheit > input");
 const kelvinInput = document.querySelector("#kelvin > input");
 
-celsiusInput.addEventListener('input', function() {
-});
+
+const celsiusToFaAndKe = () => {
+  const celsiusTemp = parseFloat(celsiusInput.value);
+  const fahrenheitTemp = (celsiusTemp * 1.8) + 32;
+  const kelvinTemp = celsiusTemp + 273.15;
+
+  fahrenheitInput.value = fahrenheitTemp;
+  kelvinInput.value = kelvinTemp;
+};
+
+
+// const fahrenheitToCeAndKe = () => {
+//   const fahrenheitTemp = parseFloat(fahrenheitInput.value);
+//   const celsiusTemp =
+//   const kelvinTemp =
+// };
+
+celsiusInput.addEventListener('input', celsiusToFaAndKe);
