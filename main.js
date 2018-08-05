b85b3925a13dce87a822c8c8b8012c49218cd6d0
Elle H <itsellej@gmail.com>
2018-08-05 15:30:03 +0100

    Fix minor syntax errors, center footer, add README.md

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const celsiusInput = document.querySelector("#celsius > input");
-const fahrenheitInput = document.querySelector("#fahrenheit > input");
-const kelvinInput = document.querySelector("#kelvin > input");
+const celsiusInput = document.querySelector('#celsius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
 
 const roundToTwoDP = (num) => {
   return num.toFixed(2);
