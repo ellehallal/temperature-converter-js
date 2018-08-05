f5f8f9f91df17145a086b0206f2f42145136b943
Elle H <itsellej@gmail.com>
2018-08-05 14:08:58 +0100

    Add input fields as consts

---
+++ main.js
@@ -0,0 +1,6 @@
+const celsiusInput = document.querySelector("#celsius > input");
+const fahrenheitInput = document.querySelector("#fahrenheit > input");
+const kelvinInput = document.querySelector("#kelvin > input");
+
+celsiusInput.addEventListener('input', function() {
+});
